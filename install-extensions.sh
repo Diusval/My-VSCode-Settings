d8da41c26b637353f6303c365e90d2efd5727b44
Diusval Gonçalves Pinheiro <79423948+Diusval@users.noreply.github.com>
2023-07-21 21:37:55 -0300

    Add files via upload

---
+++ install-extensions.sh
@@ -0,0 +1,14 @@
+code --install-extension njpwerner.autodocstring &&
+code --install-extension streetsidesoftware.code-spell-checker-portuguese-brazilian &&
+code --install-extension formulahendry.code-runner &&
+code --install-extension streetsidesoftware.code-spell-checker &&
+code --install-extension aeschli.vscode-css-formatter &&
+code --install-extension EditorConfig.EditorConfig &&
+code --install-extension GitHub.copilot &&
+code --install-extension ritwickdey.LiveServer &&
+code --install-extension PKief.material-icon-theme &&
+code --install-extension omthemes.omthemes &&
+code --install-extension esbenp.prettier-vscode &&
+code --install-extension ms-python.python &&
+code --install-extension KevinRose.vsc-python-indent &&
+code --install-extension natqe.reload
