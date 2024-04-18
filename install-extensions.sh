8d9ae6adfd76528e8cc978daa73c2354334f336d
Diusval Gonçalves Pinheiro <79423948+Diusval@users.noreply.github.com>
2024-04-17 22:50:37 -0300

    Update install-extensions.sh

--- install-extensions.sh
+++ install-extensions.sh
@@ -1,31 +1,17 @@
-code --install-extension njpwerner.autodocstring &&
-code --install-extension streetsidesoftware.code-spell-checker-portuguese-brazilian &&
-code --install-extension formulahendry.code-runner &&
-code --install-extension streetsidesoftware.code-spell-checker &&
-code --install-extension aeschli.vscode-css-formatter &&
-code --install-extension EditorConfig.EditorConfig &&
-code --install-extension GitHub.copilot &&
-code --install-extension ritwickdey.LiveServer &&
-code --install-extension PKief.material-icon-theme &&
-code --install-extension omthemes.omthemes &&
-code --install-extension esbenp.prettier-vscode &&
+code --install-extension drcika.apc-extension &&
+code --install-extension miguelsolorio.min-theme &&
+code --install-extension miguelsolorio.symbols &&
 code --install-extension ms-python.python &&
+code --install-extension natqe.reload &&
+code --install-extension GitHub.copilot &&
 code --install-extension KevinRose.vsc-python-indent &&
-code --install-extension natqe.reload
 
 or
 
-code --install-extension njpwerner.autodocstring |
-code --install-extension streetsidesoftware.code-spell-checker-portuguese-brazilian |
-code --install-extension formulahendry.code-runner |
-code --install-extension streetsidesoftware.code-spell-checker |
-code --install-extension aeschli.vscode-css-formatter |
-code --install-extension EditorConfig.EditorConfig |
-code --install-extension GitHub.copilot |
-code --install-extension ritwickdey.LiveServer |
-code --install-extension PKief.material-icon-theme |
-code --install-extension omthemes.omthemes |
-code --install-extension esbenp.prettier-vscode |
+code --install-extension drcika.apc-extension |
+code --install-extension miguelsolorio.min-theme |
+code --install-extension miguelsolorio.symbols |
 code --install-extension ms-python.python |
+code --install-extension natqe.reload |
+code --install-extension GitHub.copilot |
 code --install-extension KevinRose.vsc-python-indent |
-code --install-extension natqe.reload
